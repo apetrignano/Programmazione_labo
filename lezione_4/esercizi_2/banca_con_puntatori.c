00f7358e06adadf4d73483a6ace275ef07a39473
Angelo Petrignano <angelopetrignano@MacBook-Air-di-Angelo.local>
2025-11-11 13:24:15 +0100

    exxxx

---
+++ lezione_4/esercizi_2/banca_con_puntatori.c
@@ -0,0 +1,11 @@
+##include <stdlib.h>
+#include <stdio.h>
+
+int main() {
+
+  do{
+    printf("1.\tVisualizza saldo.\n2.\tDeposito.\n3.\tPrelievo.\n4.\tCalcolo interesse.\n5.\tEsci.\n> ");
+  } while();
+
+  return 0;
+}
