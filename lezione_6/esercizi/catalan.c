66b2a04946a1c1c88a82ba055d931e505affde8a
Angelo Petrignano <angelopetrignano@MacBook-Air-di-Angelo.fritz.box>
2025-11-27 15:51:28 +0100

    added catalan.c

---
+++ lezione_6/esercizi/catalan.c
@@ -0,0 +1,32 @@
+#include <stdlib.h>
+#include <stdio.h>
+
+int catalan(int);
+
+int main(int argc, char *argv[]) {
+
+  if(argc != 2 || atoi(argv[1]) < 0) {
+    printf("Usage: %s <num_maggiore_di_0\n", argv[0]);
+    return -1;
+  }
+
+  int n = atoi(argv[1]);
+
+  printf("C(%d) = %d\n", n, catalan(n));
+
+  return 0;
+}
+
+int catalan(int n) {
+  //printf("si parte con %d\n", n);
+
+  if(n == 0) {
+    //printf("raggiunto caso base c(%d)\n", n);
+    
+    return 1;
+  }
+
+  //printf("passo ricorsivo: C(n-1) = %d\n", 2 * (2 * n - 1) / (n + 1) * catalan(n - 1));
+  printf("chiamata alla funzione c(%d)\n", n - 1);
+  return (int) 2 * ((2 * (double)n) - 1) / ((double)n + 1) * catalan(n - 1);
+}
